e498f337299a7545b17f8dac5ef095dbd194245b
hugo valois <hvgaming@yahoo.com>
2018-10-09 20:40:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,53 +1,16 @@
 class CashRegister
   def initialize
-    @bills = []
+    
   end
-
-  def each(&block)
-    @bills.each(&block)
-  end
-
-  def product_frequency
-    result = {} 
-    each { |bill| 
-      bill.each { |position| 
-        result[position.product] ||= 0 
-        result[position.product] += position.quantity 
-      } 
-    } 
-    result 
-  end
-
-  def << bill
-    @bills << bill
-    self
-  end
+def assert_not_equal 
+end
+def add_item
+end
+def apply_discount
+end
+def items
+end
+def void_last_transaction
 end
 
-class Bill
-  attr_reader :positions,:nr
-  protected :positions
-
-  def initialize(nr)
-    @nr = nr
-    @positions = []
-  end
-
-  def each(&block)
-    @positions.each(&block)
-  end
-
-  def << pos
-    @positions << pos
-    self
-  end
-end
-
-class Position
-  attr_reader :product,:quantity,:price
-  def initialize(product,quantity,single_price)
-    @product = product
-    @quantity = quantity
-    @price = single_price * quantity
-  end
 end  
