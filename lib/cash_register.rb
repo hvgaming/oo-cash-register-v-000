6c139f9477be8201fa905afad7ea86f305f6fef3
hugo valois <hvgaming@yahoo.com>
2018-10-09 20:41:25 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,1 @@
-class CashRegister
-  def initialize
-    
-  end
-def assert_not_equal 
-end
-def add_item
-end
-def apply_discount
-end
-def items
-end
-def void_last_transaction
-end
-
-end  
+ 
